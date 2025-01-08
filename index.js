d2f2863d9bfb381661f32f65ff3bb47d6e82405a
TrapFish <92753280+TrapFish@users.noreply.github.com>
2025-01-09 00:27:23 +0530

    checking the frequency od the character in a string

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
-//sum zero problem
+//Anagram check
 
-// here it is with o(n2)
-let arr = [-5, -4,-3,-2, 0, 2,4,6 , 8];
-function cal(arr){
-  let left = arr[0];
-  let right = arr.length - 1;
-  
-  while(left < right){
-      sum = arr[left] + arr[right];
-      if(sum === 0){
-          return [arr[left], arr[right]];
-      } else if(sum>0){
-          right --;
-      } else {
-          left ++;
-      }
-  }
-    
+let stringOne = "Kumar Gourav";
+
+let obj= {};
+
+let stringToArray = [...stringOne.toLowerCase()];
+
+for(let char of stringToArray){
+    if(char !== ' '){
+        obj[char] = (obj[char] || 0) +1;
+    }
 }
-let val =  cal(arr);
-console.log("Line 15 ", val)
+
+console.log(obj)
