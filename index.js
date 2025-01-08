4db2be1668eaceb113eb8ff28e96643ff5da3912
TrapFish <92753280+TrapFish@users.noreply.github.com>
2025-01-09 00:16:51 +0530

    o(n) linear time complexity

--- index.js
+++ index.js
@@ -3,13 +3,19 @@
 // here it is with o(n2)
 let arr = [-5, -4,-3,-2, 0, 2,4,6 , 8];
 function cal(arr){
-    for(let i = 0; i < arr.length - 1; i++){
-    for(let j = 1; j < arr.length -1; j++){
-        if(arr[i] + arr[j] === 0){
-            return [arr[i], arr[j]]
-        }
-    }
-}
+  let left = arr[0];
+  let right = arr.length - 1;
+  
+  while(left < right){
+      sum = arr[left] + arr[right];
+      if(sum === 0){
+          return [arr[left], arr[right]];
+      } else if(sum>0){
+          right --;
+      } else {
+          left ++;
+      }
+  }
     
 }
 let val =  cal(arr);
