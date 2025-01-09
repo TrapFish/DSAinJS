16a1883820f58ca3701aa135fb3921b214014bb7
TrapFish <92753280+TrapFish@users.noreply.github.com>
2025-01-09 17:00:54 +0530

    count of the largest consecutive number

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
-let arr = [1,1,2,2,3,4,4,5,6,7,8,8];
+//count of the largest consecutive number
 
 
-let newArr = arr.reduce((acc, value)=> {
- 
-    if(!acc.includes(value)){
-        acc.push(value);
+function largestContinuousNumber(arr, n){
+    let arrLength = arr.length;
+    if(n > arrLength) return false;
+    let numOfContinousSubArray = arrLength - n + 1;
+    let max = 0;
+    let subArarys= [];
+
+    for(let i = 0; i < numOfContinousSubArray; i++){
+      subArarys.push(arr.slice(i , i + n));
     }
 
-    return acc;
-} , []);
-console.log(newArr, newArr.length)
+    for(let j =0 ; j < subArarys.length; j++){
+       let sum = subArarys[j].reduce((acc, value)=>{
+          return acc+=value;
+       }, 0)
+       console.log("Sum is ::", sum)
+       if(sum > max){
+        max = sum;
+       }
+    }
+    console.log("max is ::" , max)
+    console.log("Line 5 :: ", arr, n, arrLength, numOfContinousSubArray,subArarys)
+   
+}
+
+let arr=[1,2,3,4,3,5,4,6,7,8];
+let n = 4;
+largestContinuousNumber(arr,n)
