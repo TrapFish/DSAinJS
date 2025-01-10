7f5ae8b6e596b741ee65c305ffc24c4c9cac8509
TrapFish <92753280+TrapFish@users.noreply.github.com>
2025-01-10 15:13:16 +0530

    Push the sort value

--- index.js
+++ index.js
@@ -1,28 +1,10 @@
-function sortArray(arr) {
-  if (arr.length <= 1) {
-    return arr; // Base case: array with 0 or 1 element is already sorted
-  }
- 
-  const middleValue = arr[Math.floor(Math.random() * arr.length)];
-  
-  const less = [];
-  const great = [];
-  const mid = [];
+const unsortedArray = [64, 34, 25, 12, 22, 11, 90];
 
-  for(const num of arr){
-    if(num < middleValue){
-      less.push(num);
-    }else if(num > middleValue){
-      great.push(num);
-    }else{
-      mid.push(middleValue)
-    }
-  }
-
-  return [...sortArray(less) , ...mid , ...sortArray(great)]
-}
+const sortedArray = unsortedArray.sort((a,b)=>{
+  return a-b;
+})
 
 // Example usage:
-const unsortedArray = [64, 34, 25, 12, 22, 11, 90];
-const sortedArray = sortArray(unsortedArray);
+
+// const sortedArray = sortArray(unsortedArray);
 console.log("Sorted array:", sortedArray);
