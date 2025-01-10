bfc4e01f83fcf856553e4439a8ac4af88f955d8b
TrapFish <92753280+TrapFish@users.noreply.github.com>
2025-01-10 16:29:12 +0530

    changes for higher recussive function

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const unsortedArray = [64, 34, 25, 12, 22, 11, 90];
+function findOddUsingRecursive(inArra){
+  let result = [];
+  function helperRecursiveFunction(inarrray){
+    if(inarrray.length === 0){
+       return ;
+    } else {
+        let firstArray = inarrray[0];
+        if(firstArray % 2 !== 0){
+          result.push(firstArray);
+        }
+        helperRecursiveFunction(inarrray.slice(1));
+    }
 
-const sortedArray = unsortedArray.sort((a,b)=>{
-  return a-b;
-})
+  }
+  helperRecursiveFunction(inArra);
+  console.log("line 16 ::", result)
+  return result;
+}
 
-// Example usage:
-
-// const sortedArray = sortArray(unsortedArray);
-console.log("Sorted array:", sortedArray);
+let imnArray = [1,2,3,4,5, 6,7,8,9,10];
+findOddUsingRecursive(imnArray);
+//let outArray = [1,3,5,7,9]
