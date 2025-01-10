46c30e08e1e4b47b533e6b2a02cb3fcf73a87a5d
TrapFish <92753280+TrapFish@users.noreply.github.com>
2025-01-10 09:31:56 +0530

    recursive concept with currying function

--- index.js
+++ index.js
@@ -1,32 +1,13 @@
-//checking square in array
-// [1,2,3,4]-> [1, 9, 4,16]
+//curring function with recursive function
 
-
-function arrayCheck(val, valA){
-    let map1 = {};
-    let map2 = {};
-
-    for(let i of val){
-      map1[i] = (map1[i] || 0) + 1;
-    }
-    for(let i of valA){
-      map2[i] = (map2[i] || 0) + 1;
-    }
-
-    console.log("Line 16 ::", map1, map2)
-
-    for(key in map1){
-      if(!map2[key * key]){
-        return false;
-      }
-      if(map1[key] !== map2[key * key]){
-        return false;
-      }
-    }
-    return true;
+function add(a){
+  return function (b){
+     if(b){
+       return add(a+b);
+     } else{
+      return a;
+     }
+  }
 }
 
-let arr1= [1,2,3,4];
-let arr2 =[1,4,8,16];
-
-console.log(arrayCheck(arr1, arr2));
+console.log(add(2)(3)(8)());
