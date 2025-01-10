47c478ee2d8e3cb24bc2b10aa38793e2155eb05c
TrapFish <92753280+TrapFish@users.noreply.github.com>
2025-01-10 10:39:03 +0530

    changes for the file

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
-//curring function with recursive function
+//sorting an array with recursion
 
-function add(a){
-  return function (b){
-     if(b){
-       return add(a+b);
-     } else{
-      return a;
-     }
-  }
+function sortingWithRecursion(arr){
+   if(arr.length <= 1){
+     return arr;
+   }
+
+   let middle = Math.floor(arr.length / 2);
+   let leftHalf = arr.slice(0, middle);
+   let rightHalf = arr.slice(middle);
+
+   return merge(leftHalf, rightHalf);
 }
 
-console.log(add(2)(3)(8)());
+
+function merge(leftHalfPortion, rightHalfPortion){
+     let result = [];
+     let leftIndex = 0;
+     let rightIndex = 0;
+
+     while(leftIndex < leftHalfPortion.length && rightIndex < rightHalfPortion.length){
+        if(leftHalfPortion[leftIndex] < rightHalfPortion[rightIndex]){
+              result.push(leftHalfPortion[leftIndex]);
+              leftIndex++;
+        }else{
+          result.push(rightHalfPortion[rightIndex]);
+          rightIndex++;
+        }
+     }
+
+     console.log("data is" ,result, leftHalfPortion, rightHalfPortion, result.concat(leftHalfPortion.slice(leftIndex)).concat(rightHalfPortion.slice(rightIndex)));
+}
+
+let arr = [64, 34, 25, 12, 22, 11, 90];
+sortingWithRecursion(arr);
