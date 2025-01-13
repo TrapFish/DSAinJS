c9da3e7627b417909c93aba5439fb9b004d2f984
TrapFish <92753280+TrapFish@users.noreply.github.com>
2025-01-13 17:12:19 +0530

    changes for the bubble sort

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-function findOddUsingRecursive(inArra){
-  let result = [];
-  function helperRecursiveFunction(inarrray){
-    if(inarrray.length === 0){
-       return ;
-    } else {
-        let firstArray = inarrray[0];
-        if(firstArray % 2 !== 0){
-          result.push(firstArray);
-        }
-        helperRecursiveFunction(inarrray.slice(1));
-    }
+//bubble sort
 
-  }
-  helperRecursiveFunction(inArra);
-  console.log("line 16 ::", result)
-  return result;
+
+function bubbleSort(arr){
+  for(let i = arr.length; i > 0 ; i--){
+    console.log("Arr is ::", arr[i]);
+    for(let j = 0; j < i-1; j++){
+      if(arr[j]>arr[j+1]){
+        [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+      }
+    }
+  } 
+  return arr;
 }
 
-let imnArray = [1,2,3,4,5, 6,7,8,9,10];
-findOddUsingRecursive(imnArray);
-//let outArray = [1,3,5,7,9]
+let a = [4,3,5,6,2,90,54,55,34];
+bubbleSort(a)
